4a1d92a1a9191958a7fe6d040faee86d67c25a15
Muliro1 <mulirokhaemba@gmail.com>
2024-05-02 04:50:38 +0300

    l

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,30 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
+'''Implementing an expiring web cache and tracker
 '''
 import redis
 import requests
-from functools import wraps
-from typing import Callable
+from datetime import timedelta
 
-
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
-
-
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
-    @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
-
-
-@data_cacher
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
+    '''This function retrieves the content of a URL, tracks the number of accesses,
+    and caches the result with an expiration time of 10 seconds.
     '''
-    return requests.get(url).text
+    if url is None or len(url.strip()) == 0:
+        return ''
+    redis_store = redis.Redis()
+    res_key = 'result:{}'.format(url)
+    req_key = 'count:{}'.format(url)
+    result = redis_store.get(res_key)
+    if result is not None:
+        redis_store.incr(req_key)
+        return result.decode('utf-8')
+    result = requests.get(url).content.decode('utf-8')
+    redis_store.setex(res_key, timedelta(seconds=10), result)
+    redis_store.incr(req_key)
+
+    return result
+
+if __name__ == "__main__":
+    # Example usage
+    url = "http://slowwly.robertomurray.co.uk"
+    print(get_page(url))
